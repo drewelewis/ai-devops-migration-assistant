e4cdbaaa09514cfe56d4a8a9b0fd8a78a2a624ba
Drew Lewis <29957923+drewelewis@users.noreply.github.com>
2025-06-09 19:58:26 -0400

    additional teamcity tool cleanup

--- operations/teamcity_operations.py
+++ operations/teamcity_operations.py
@@ -12,7 +12,7 @@ TEAMCITY_ADMIN_PASSWORD=os.getenv('TEAMCITY_ADMIN_PASSWORD')
 
 
 class TeamcityOperations():
-    def get_projects(self) -> list[str]:
+    def get_project_ids(self) -> list[str]:
         try:
             project_ids: List[str] = []
             tc = TeamCity(TEAMCITY_URL, auth=(TEAMCITY_ADMIN, TEAMCITY_ADMIN_PASSWORD))
@@ -30,44 +30,44 @@ class TeamcityOperations():
         finally:
             pass
 
-    def get_all_builds(self) -> str:
-        try:
-            tc = TeamCity(TEAMCITY_URL, auth=(TEAMCITY_ADMIN, TEAMCITY_ADMIN_PASSWORD))
-            # connect to TeamCity server
-            builds = tc.builds.get_all_builds()
-            return builds
-        except Exception as e:
-            print(f"An error occurred with TeamcityOperations.ge_builds: {e}")
-            return []
-        finally:
-            pass
+    # def get_all_builds(self) -> str:
+    #     try:
+    #         tc = TeamCity(TEAMCITY_URL, auth=(TEAMCITY_ADMIN, TEAMCITY_ADMIN_PASSWORD))
+    #         # connect to TeamCity server
+    #         builds = tc.builds.get_all_builds()
+    #         return builds
+    #     except Exception as e:
+    #         print(f"An error occurred with TeamcityOperations.ge_builds: {e}")
+    #         return []
+    #     finally:
+    #         pass
 
-    # get all builds by project
-    def get_builds_by_project(self, project_id: str) -> str:
-        try:
-            tc = TeamCity(TEAMCITY_URL, auth=(TEAMCITY_ADMIN, TEAMCITY_ADMIN_PASSWORD))
-            # connect to TeamCity server
-            builds = tc.builds.get_builds(project=project_id)
-            return builds
-        except Exception as e:
-            print(f"An error occurred with TeamcityOperations.get_builds_by_project: {e}")
-            return []
-        finally:
-            pass
+    # # get all builds by project
+    # def get_builds_by_project(self, project_id: str) -> str:
+    #     try:
+    #         tc = TeamCity(TEAMCITY_URL, auth=(TEAMCITY_ADMIN, TEAMCITY_ADMIN_PASSWORD))
+    #         # connect to TeamCity server
+    #         builds = tc.builds.get_builds(project=project_id)
+    #         return builds
+    #     except Exception as e:
+    #         print(f"An error occurred with TeamcityOperations.get_builds_by_project: {e}")
+    #         return []
+    #     finally:
+    #         pass
 
-    # get build definitions by build configuration id
-    def get_build_definitions_by_build_config_id(self, build_config_id: str) -> str:
-        try:
-            tc = TeamCity(TEAMCITY_URL, auth=(TEAMCITY_ADMIN, TEAMCITY_ADMIN_PASSWORD))
-            # connect to TeamCity server
-            build_type_locator= f"id:{build_config_id}"
-            build_definitions = tc.build_type_api.get(build_type_locator) 
-            return build_definitions
-        except Exception as e:
-            print(f"An error occurred with TeamcityOperations.get_build_definitions_by_project: {e}")
-            return []
-        finally:
-            pass
+    #  get build definitions by build configuration id
+    # def get_build_definitions_by_build_config_id(self, build_config_id: str) -> str:
+    #     try:
+    #         tc = TeamCity(TEAMCITY_URL, auth=(TEAMCITY_ADMIN, TEAMCITY_ADMIN_PASSWORD))
+    #         # connect to TeamCity server
+    #         build_type_locator= f"id:{build_config_id}"
+    #         build_definitions = tc.build_type_api.get(build_type_locator) 
+    #         return build_definitions
+    #     except Exception as e:
+    #         print(f"An error occurred with TeamcityOperations.get_build_definitions_by_project: {e}")
+    #         return []
+    #     finally:
+    #         pass
 
     # get build definitions by project name
     def get_build_definitions_by_project_id(self, project_id: str) -> str:
@@ -83,32 +83,20 @@ class TeamcityOperations():
         finally:
             pass
 
-    # get build steps by build configuration id
-    def get_build_steps_by_build_config_id(self, build_config_id: str) -> str:
-        try:
-            tc = TeamCity(TEAMCITY_URL, auth=(TEAMCITY_ADMIN, TEAMCITY_ADMIN_PASSWORD))
-            # connect to TeamCity server
-            build_steps = tc.builds.get_build_steps(build_config_id)
-            return build_steps
-        except Exception as e:
-            print(f"An error occurred with TeamcityOperations.get_build_steps_by_build_config_id: {e}")
-            return []
-        finally:
-            pass
 
-TeamcityOperations = TeamcityOperations()
+# TeamcityOperations = TeamcityOperations()
 
 # builds=TeamcityOperations.get_all_builds()
 # print (builds)
 
-projects=TeamcityOperations.get_projects()
-print(projects)
+# projects=TeamcityOperations.get_projects()
+# print(projects)
 
 # definitions=TeamcityOperations.get_build_definitions_by_build_config_id("Payroll_Build")
 # print(definitions)
 
-definitions=TeamcityOperations.get_build_definitions_by_project_id("Payroll")
-print (definitions)
+# definitions=TeamcityOperations.get_build_definitions_by_project_id("Payroll")
+# print (definitions)
 
 # definitions=TeamcityOperations.get_build_definitions_by_project_name("SpringBootApplicationExample")
 # print (definitions)
--- tools/teamcity_tools.py
+++ tools/teamcity_tools.py
@@ -20,23 +20,36 @@ class TeamCityTools():
         return_direct: bool = True
                
         def _run(self) -> str:
-            repos=teamcity_Operations.get_projects()
-            return str(repos)
+            project_ids=teamcity_Operations.get_project_ids()
+            return str(project_ids)
     
-    class TeamCityGetAllBuildsTool(BaseTool):
-            name: str = "TeamCityGetAllBuildsTool"
-            description: str = """
-                useful for when you need to get a list of all builds from a TeamCity server.
-            """.strip()
-            return_direct: bool = False
+    class TeamCityGetBuildDefinitionsByProjectId(BaseTool):
+        name: str = "TeamCityGetBuildDefinitionsByProjectId"
+        description: str = """
+            useful for when you need a build definition by project id from a TeamCity server.
+        """.strip()
+        return_direct: bool = False
 
-            def _run(self) -> str:
-                builds=teamcity_Operations.get_all_builds()
-                return str(builds)
+        class TeamCityGetBuildDefinitionsByProjectIdInputModel(BaseModel):
+            project_id: str = Field(description="project_id")
+
+            # Validation method to check parameter input from agent
+            @field_validator("project_id")
+            def validate_query_param(project_id):
+                if not project_id:
+                    raise ValueError("TeamCityGetBuildDefinitionsByProjectId error: project_id parameter is empty")
+                else:
+                    return project_id
+                
+        args_schema: Optional[ArgsSchema] = TeamCityGetBuildDefinitionsByProjectIdInputModel
+    
+        def _run(self, project_id: str) -> str:
+            builds=teamcity_Operations.get_build_definitions_by_project_id(project_id)
+            return str(builds)
 
     # Init above tools and make available
     def __init__(self) -> None:
-        self.tools = [self.TeamCityGetProjectsTool(), self.TeamCityGetAllBuildsTool()]
+        self.tools = [self.TeamCityGetProjectsTool(), self.TeamCityGetBuildDefinitionsByProjectId()]
 
     # Method to get tools (for ease of use, made so class works similarly to LangChain toolkits)
     def tools(self) -> List[BaseTool]:
